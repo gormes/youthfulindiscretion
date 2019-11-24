70f74d377abf65f83970546fe75093b55aee2c28
Gwyneth Ormes <gormes@wpi.edu>
2019-11-24 16:59:16 -0500

    List All Video Segments Handler [DOESN'T COMPILE]

---
+++ src/main/java/player/handle/ListAllVideoSegmentHandler.java
@@ -0,0 +1,57 @@
+package player.handle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import player.db.*;
+import player.model.*;
+
+public class ListAllVideoSegmentHandler implements RequestHandler<S3Event, String> {
+
+	public LambdaLogger logger;
+	
+    private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+
+    public ListAllVideoSegmentHandler() {}
+
+    // Test purpose only.
+    ListAllVideoSegmentHandler(AmazonS3 s3) {
+        this.s3 = s3;
+    }
+
+    @Override
+    public String handleRequest(S3Event event, Context context) {
+        context.getLogger().log("Received event: " + event);
+
+        // Get the object from the event and show its content type
+        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
+        String key = event.getRecords().get(0).getS3().getObject().getKey();
+        try {
+            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
+            String contentType = response.getObjectMetadata().getContentType();
+            context.getLogger().log("CONTENT TYPE: " + contentType);
+            return contentType;
+        } catch (Exception e) {
+            e.printStackTrace();
+            context.getLogger().log(String.format(
+                "Error getting object %s from bucket %s. Make sure they exist and"
+                + " your bucket is in the same region as this function.", key, bucket));
+            throw e;
+        }
+    }
+}
