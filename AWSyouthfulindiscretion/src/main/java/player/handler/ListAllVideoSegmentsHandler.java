b5f87f8e6d3c3db1cb82705e730bd1abbcf05e73
Gwyneth Ormes <gormes@wpi.edu>
2019-11-25 15:04:47 -0500

    List all video segments but without the logger

--- AWSyouthfulindiscretion/src/main/java/player/handler/ListAllVideoSegmentsHandler.java
+++ AWSyouthfulindiscretion/src/main/java/player/handler/ListAllVideoSegmentsHandler.java
@@ -22,8 +22,6 @@ import player.model.*;
 import player.db.*;
 
 public class ListAllVideoSegmentsHandler implements RequestHandler<S3Event, String> {
-
-	public LambdaLogger logger;
    
 	private AmazonS3 s3 = null;
 
@@ -35,37 +33,27 @@ public class ListAllVideoSegmentsHandler implements RequestHandler<S3Event, Stri
     }
 
     List<VideoSegment> listAllVideoSegments() throws Exception{
-    	//logger.log("in listAllVideoSegments");
     	VideoSegmentDAO dao = new VideoSegmentDAO();
     	
     	return dao.getAllVideoSegments();
     }
     
     List<VideoSegment> listAllVideoSegmentsS3() throws Exception {
-    	//logger.log("in listAllVideoSegmentsS3");
     	if(s3 == null) {
-    		//logger.log("attach to S3 request");
     		s3 = s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
-    		//logger.log("attach to S3 to succeed");
     	}
     	
     	ArrayList<VideoSegment> videoSeg = new ArrayList<>();
-    	//https://3733youthfulindiscretion.s3.us-east-2.amazonaws.com/videoSegments/Are+you(Kirk).mp4
     	ListObjectsV2Request listAllObjRequest = new ListObjectsV2Request().withBucketName("3733youthfulindiscretion").withPrefix("videoSegments");
     	
-    	//logger.log("process request");
-    	
     	ListObjectsV2Result result = s3.listObjectsV2(listAllObjRequest);
-    	//logger.log("process request succeeded");
-    	
+  
     	
     	List<S3ObjectSummary> objects =  result.getObjectSummaries();
     	
 		for (S3ObjectSummary os: objects) {
 		      String name = os.getKey();
-			//  logger.log("S3 found:" + name);
-
-		      // If name ends with slash it is the 'constants/' bucket itself so you skip
+		      
 		      if (name.endsWith("/")) { continue; }
 				
 		      S3Object obj = s3.getObject("3733youthfulindiscretion", name);
@@ -75,11 +63,9 @@ public class ListAllVideoSegmentsHandler implements RequestHandler<S3Event, Stri
 					String val = sc.nextLine();
 					sc.close();
 					
-					// just grab name *after* the slash. Note this is a SYSTEM constant
 					int postSlash = name.indexOf('/');
 					videoSeg.add(new VideoSegment("Actor", "Phrase", "https://3733youthfulindiscretion.s3.us-east-2.amazonaws.com/videoSegments/" + name));
 				} catch (Exception e) {
-				//	logger.log("Unable to parse contents of " + name);
 				}
 		    }
 			return videoSeg;
