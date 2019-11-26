80461f6622760d7bf16d5a3971f862c7583307c7
lindbergsimpson <lindbergsimpson@localhost>
2019-11-26 13:41:41 -0500

    Cleaned html file and changed video type to ogg

---
+++ AWSyouthfulindiscretion/js/adminSites.js
@@ -0,0 +1,76 @@
+function refreshSites() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", lists_url, true);
+	xhr.send();
+
+	console.log("sent");
+
+	// This will process results and update HTML as appropriate. 
+	xhr.onloadend = function () {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			console.log ("XHR:" + xhr.responseText);
+			processListResponseS(xhr.responseText);
+		} else {
+			processListResponseS("N/A");
+		}
+	};
+}
+
+
+//TODO
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+function processListResponseS(result) {
+	console.log("res:" + result);
+	// Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+	var js = JSON.parse(result);
+	var constList = document.getElementById('unorderedSites');
+
+	var output = "";
+	for (var i = 0; i < js.list.length; i++) {
+		var constantJson = js.list[i];
+		console.log(constantJson);
+
+		var url = constantJson["url"];
+		output = output + "<div id=\"const" + url + "\"><p>"+url + "<input type=\"button\" value=\"Delete Site\" onClick=\"alert('Unable to delete')\"> </input>" + "</p> </div>";
+	}
+
+	// Update computation result
+	constList.innerHTML = output;
+}
+
+//Function to add site to list of registered sites
+function addSite() {
+	//Create variable to store site entered in the URL space
+	var newSite = document.getElementById("registeredURL").value;
+	if (newSite!=""){
+		/* Create a list item element and add inner HTML from the text entered in the URL box*/
+		var ns=document.createElement("li"); 	
+		ns.innerHTML = newSite;
+		/* Reference list of sites then add new list element as a child to the list.*/
+		var los = document.getElementById("oderedSites");
+		los.appendChild(ns);
+	}
+	else{
+		//Error message if nothing is enetered into URL space
+		alert('Please input a URL for a remote site');
+	}
+}
+//Function to delete site from the list of registered sites
+function deleteSite() {
+	//Reference to the list of registered remote sites
+	var los = document.getElementById("oderedSites");
+	//reference to last site added to the list
+	var lastChild = los.lastChild;
+	if(lastChild!=null){
+		los.removeChild(lastChild);
+	}
+	else {
+		//Error message if list of sites is empty
+		alert('No remote sites currently registered');
+	}
+
+}
---
+++ AWSyouthfulindiscretion/js/adminVideoSegments.js
@@ -0,0 +1,64 @@
+function initialize(){
+	refreshVideoSegments();
+	refreshSites();
+}
+
+function refreshVideoSegments() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", listv_url, true);
+	xhr.send();
+
+	console.log("sent");
+
+	// This will process results and update HTML as appropriate. 
+	xhr.onloadend = function () {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			console.log ("XHR:" + xhr.responseText);
+			processListResponseV(xhr.responseText);
+		} else {
+			processListResponseV("N/A");
+		}
+	};
+}
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+function processListResponseV(result) {
+	console.log("res:" + result);
+	// Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+	var js = JSON.parse(result);
+	var constList = document.getElementById('VideoSegments');
+
+	var output = "";
+	for (var i = 0; i < js.list.length; i++) {
+		var constantJson = js.list[i];
+		console.log(constantJson);
+
+		var actor = constantJson["actor"];
+		var phrase = constantJson["phrase"];
+		var url = constantJson["url"];
+		var id = constantJson["id"];
+		output = output + "<div id=\"video" + actor + "" + phrase+ "\"><b>" + "<video id=\"video1\" width=\"300\" height=\"220\" controls> <source src=\""+url+"\"type=\"video/ogg\" </video> </b> </div>";
+		output = output + "<div id=\"button" + actor + "" + phrase+ "\"><b>" + "<input type=\"button\" value=\"Marked\" onClick=\"toggleMark(this)\"> </input> </b> </div>";
+	}
+
+	// Update computation result
+	constList.innerHTML = output;
+}
+
+
+//Function to toggle Marked/Unmarked status of the video segments
+function toggleMark(tag) {
+		//Flip status when function is called
+	if (tag.value === "Marked") {
+		tag.value = "Unmarked";
+		tag.style.backgroundColor = "red";
+		} else {
+		tag.value = "Marked";
+		tag.style.backgroundColor = "#78bfbf";
+			 }
+}
+
