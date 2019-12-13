8eefdb60d5739a01acd0d0f896a2ffb25546d793
Gwyneth Ormes <gormes@wpi.edu>
2019-12-13 01:47:32 -0500

    Request Testing (81.2% Coverage)

--- AWSyouthfulindiscretion/src/test/java/player/InstantiateRequestResponse.java
+++ AWSyouthfulindiscretion/src/test/java/player/InstantiateRequestResponse.java
@@ -1,5 +1,7 @@
 package player;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -46,6 +48,10 @@ public class InstantiateRequestResponse {
 		
 		AppendVideoSegmentRequest appendVideoSegRequestNormal = new AppendVideoSegmentRequest("vsurl", "pid");
 		AppendVideoSegmentRequest appendVideoSegRequestError = new AppendVideoSegmentRequest();
+		appendVideoSegRequestNormal.setPlaylistID("playlist test");
+		appendVideoSegRequestNormal.setVideoSegmentID("video segment test");
+		assertEquals(appendVideoSegRequestNormal.getPlaylistID().equals("playlist test"), true);
+		assertEquals(appendVideoSegRequestNormal.getVideoSegmentID().equals("video segment test"), true);
 		
 		AppendVideoSegmentResponse appendVideoSegResponseNormal = new AppendVideoSegmentResponse("response");
 		AppendVideoSegmentResponse appendVideoSegReponseEmpty = new AppendVideoSegmentResponse("error message", 400);
@@ -55,6 +61,14 @@ public class InstantiateRequestResponse {
 		
 		CreateVideoSegmentRequest createVideoSegRequestNormal = new CreateVideoSegmentRequest("filename", "actor", "phrase", "encoded");
 		CreateVideoSegmentRequest createVideoSegRequestEmpty = new CreateVideoSegmentRequest();
+		createVideoSegRequestNormal.setFileName("file test");
+		createVideoSegRequestNormal.setActor("actor test");
+		createVideoSegRequestNormal.setPhrase("phrase test");
+		createVideoSegRequestNormal.setEncodedContents("contents test");
+		assertEquals(createVideoSegRequestNormal.getFileName().equals("file test"), true);
+		assertEquals(createVideoSegRequestNormal.actor().equals("actor test"), true);
+		assertEquals(createVideoSegRequestNormal.phrase().equals("phrase test"), true);
+		assertEquals(createVideoSegRequestNormal.encodedContents().equals("contents test"), true);
 		
 		CreateVideoSegmentResponse createVideoSegmentResponseNormal = new CreateVideoSegmentResponse(null, 200);
 		CreateVideoSegmentResponse createVideoSegmentResponesError = new CreateVideoSegmentResponse(400, "error message");
@@ -64,6 +78,8 @@ public class InstantiateRequestResponse {
 		
 		DeleteVideoSegmentRequest deleteVideoSegmentRequestNormal = new DeleteVideoSegmentRequest("url");
 		DeleteVideoSegmentRequest deleteVideoSegmentRequestEmpty = new DeleteVideoSegmentRequest();
+		deleteVideoSegmentRequestNormal.setvsId("vs test");
+		assertEquals(deleteVideoSegmentRequestNormal.getvsID().equals("vs test"), true);
 		
 		DeleteVideoSegmentResponse deleteVideoSegmentResponseNormal = new DeleteVideoSegmentResponse("message", 200);
 		DeleteVideoSegmentResponse deleteVideoSegmentResponseEmpty = new DeleteVideoSegmentResponse("response", 400, "error message");
@@ -72,25 +88,43 @@ public class InstantiateRequestResponse {
 		PlaylistRequest playlistRequestWithName = new PlaylistRequest("name");
 		
 		RemoteSegmentResponseObject remoteSegmentResponse = new RemoteSegmentResponseObject("url", "character", "text");
+		assertEquals(remoteSegmentResponse.getUrl().equals("url"), true);
+		assertEquals(remoteSegmentResponse.getCharacter().equals("character"), true);
+		assertEquals(remoteSegmentResponse.getText().equals("text"), true);
+		
 		
 		RemoveSiteRequest removeSiteRequestNormal = new RemoveSiteRequest("url");
 		RemoveSiteRequest removeSiteRequestEmpty = new RemoveSiteRequest();
+		removeSiteRequestNormal.setID("url test");
+		assertEquals(removeSiteRequestNormal.getID().equals("url test"), true);
 		
 		RemoveSiteResponse removeSiteResponseNormal = new RemoveSiteResponse("response", 200);
 		RemoveSiteResponse removeSiteResponseEmpty = new RemoveSiteResponse("response", 400, "error message");
 		
 		RemoveVideoSegmentPlaylistRequest removeVideoSegmentRequestNormal = new RemoveVideoSegmentPlaylistRequest("PlaylistID", "VideoSegmentURL");
+		RemoveVideoSegmentPlaylistRequest removeVideoSegmentRequestError = new RemoveVideoSegmentPlaylistRequest();
+		removeVideoSegmentRequestNormal.setPlaylistID("playlist test");
+		removeVideoSegmentRequestNormal.setVideoSegmentURL("video segment test");
+		assertEquals(removeVideoSegmentRequestNormal.getPlaylistID().equals("playlist test") , true);
+		assertEquals(removeVideoSegmentRequestNormal.getVideoSegmentURL().equals("video segment test") , true);
 		
 		RemoveVideoSegmentPlaylistResponse removeVideoSegmentPlaylistResponseNormal = new RemoveVideoSegmentPlaylistResponse(200, "message");
+		RemoveVideoSegmentPlaylistResponse removeVideoSegmentPlaylistResponseError = new RemoveVideoSegmentPlaylistResponse();
 		
 		SiteCreateRequest siteCreateRequestNormal = new SiteCreateRequest("url");
 		SiteCreateRequest siteCreateRequestEmpty = new SiteCreateRequest();
+		siteCreateRequestNormal.setURL("test url");
+		assertEquals(siteCreateRequestNormal.getURL().equals("test url"), true);
 		
 		SiteCreateResponse siteCreateResponseNormal = new SiteCreateResponse("url", 200);
 		SiteCreateResponse siteCreateResponseEmpty = new SiteCreateResponse(400, "error message");
 		
 		VideoSegmentMarkRequest videoSegMarkRequestNormal = new VideoSegmentMarkRequest("url", false);
 		VideoSegmentMarkRequest videoSegMarkRequestEmpty = new VideoSegmentMarkRequest();
+		videoSegMarkRequestNormal.setMarked(true);
+		videoSegMarkRequestNormal.setUrl("url test");
+		assertEquals(videoSegMarkRequestNormal.getMarked(), true);
+		assertEquals(videoSegMarkRequestNormal.getUrl().equals("url test"), true);
 		
 		VideoSegmentMarkResponse videoSegMarkResponseNormal = new VideoSegmentMarkResponse(400, "messages");
 	}
