c059f3833569722dfb3ce0125f416b4a3a56bd64
Lindberg Simpson <lksimpson@wpi.edu>
2019-12-13 19:38:45 -0500

    Promise this time the search works even better

--- AWSyouthfulindiscretion/js/search.js
+++ AWSyouthfulindiscretion/js/search.js
@@ -19,7 +19,7 @@ function SearchClick(e) {
 		refreshAllSearchVideoSegments("VideoSegmentsLocal");
 		refreshAllSearchVideoSegments("VideoSegmentsRemote");
 	}
-	
+
 }
 
 function searchByCharacter(div) {
@@ -28,9 +28,9 @@ function searchByCharacter(div) {
 	var data = {};
 	data["character"] = form.character.value.toUpperCase();
 	console.log("character: " + data["character"]);
-	
+
 	parent = document.getElementById(div);
-	
+
 	for(i = 0; i < parent.childNodes.length; i++){
 		var e = parent.childNodes[i];
 		if (e.nodeType == 1){
@@ -43,19 +43,14 @@ function searchByCharacter(div) {
 		}
 	}
 
-	var foundVidList = document.getElementsByClassName(data["character"]);
-	console.log("Found vids: " + foundVidList.length)
+	var foundVidList = parent.childNodes;
+
 	for (i = 0; i < foundVidList.length; i++) {
 		var vid = foundVidList[i];
-		console.log(vid);
-		if(vid.className.split('|')[0].includes(data["character"])){
-			compare = true;
-		}
-		else {
-			compare = false;
-		}
-		if(compare){
-			vid.style.display = "block";
+		if(vid.nodeType == 1){
+			if(vid.className.split('|')[0].includes(data["character"])){
+				vid.style.display = "block";
+			}
 		}
 	}
 }
@@ -67,7 +62,7 @@ function searchByPhrase(div) {
 	console.log("phrase: " + data["phrase"]);
 
 	parent = document.getElementById(div);
-	
+
 	for(i = 0; i < parent.childNodes.length; i++){
 		var e = parent.childNodes[i];
 		if (e.nodeType == 1){
@@ -79,36 +74,15 @@ function searchByPhrase(div) {
 			}
 		}
 	}
-	var foundVidList = document.getElementsByClassName(data["phrase"]);
-	console.log("Found vids: " + foundVidList.length)
-	var compare = false;
+	var foundVidList = parent.childNodes;
+
 	for (i = 0; i < foundVidList.length; i++) {
 		var vid = foundVidList[i];
-		console.log(vid);
-		/*for (j = 1; j < vid.className.split(' ').length; j++){
-			if(vid.className.split(' ')[j]==data["phrase"].split(' ')[j-1]){
-				compare = true;
-			}
-			else {
-				compare = false;
+		if(vid.nodeType == 1){
+			if(vid.className.split('|')[1].includes(data["phrase"])){
+				vid.style.display = "block";
 			}
 		}
-		if(compare){
-			vid.style.display = "block";
-		}*/
-		
-		//for (j = 1; j < vid.className.split('|').length; j++){
-		//if(data["phrase"].includes(vid.className.split(' ')[j])){
-		//	vid.style.display = "block";
-		//}
-		
-		if(vid.className.split('|')[1].includes(data["phrase"])){
-			vid.style.display = "block";
-		}
-		
-
-	//}
-		
 	}
 }
 
@@ -121,77 +95,35 @@ function searchByBoth(div) {
 	console.log("data phrase: " + data["phrase"]);
 
 	parent = document.getElementById(div);
-	
+
 	for(i = 0; i < parent.childNodes.length; i++){
 		var e = parent.childNodes[i];
 		if (e.nodeType == 1){
 			e.style.display = "none";
 		}
 	}
-	var foundVidList = document.getElementsByClassName(data["character"] + " | " + data["phrase"]);
-	console.log("Found vids: " + foundVidList.length)
+	var foundVidList = parent.childNodes;
 	for (i = 0; i < foundVidList.length; i++) {
 		var vid = foundVidList[i];
-		console.log(vid);
-		vid.style.display = "block";
+		if(vid.nodeType==1){
+			if((vid.className.split('|')[0].includes(data["character"]))&&(vid.className.split('|')[1].includes(data["phrase"]))){
+				vid.style.display = "block";
+			}
+		}
+
 	}
 }
 
-
 function refreshAllSearchVideoSegments(div){
-	
+
 	var parent = document.getElementById(div)
-	
+
 	for(i = 0; i < parent.childNodes.length; i++){
 		var e = parent.childNodes[i];
 		if (e.nodeType == 1){
-				e.style.display = "block";
+			e.style.display = "block";
 
 		}
 	}
 }
 
-//doesn't seem to be necessary 
-/*
-//displaying the video segments 
-function refreshSearchVideoSegments() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", listv_url, true);
-	xhr.send();
-
-	console.log("sent");
-
-	// This will process results and update HTML as appropriate. 
-	xhr.onloadend = function () {
-		if (xhr.readyState == XMLHttpRequest.DONE) {
-			console.log ("XHR:" + xhr.responseText);
-			processResponseSearchVS(xhr.responseText);
-		} else {
-			processResponseSearchVS("N/A");
-		}
-	};
-}
-
-function processResponseSearchVS(result) {
-	console.log("res:" + result);
-	// Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
-	var js = JSON.parse(result);
-	var videoSegSearch= document.getElementById('VideoSegmentsSearch');
-	var output = "";
-	
-	for (var i = 0; i < js.list.length; i++) {
-		var constantJson = js.list[i];
-		console.log(constantJson);
-		
-		var actor = constantJson["actor"];
-		var phrase = constantJson["phrase"];
-		var url = constantJson["url"];
-		var id = constantJson["id"];
-		output = output + "<div id=\"vs" + actor + "" + phrase+ "\"class=\"" + actor.toUpperCase() + " " + phrase.toUpperCase() + "\">" +"<button type=\"button\" value=\""+url+"\" id=\"appendVs" + i + "\"  onClick=\"JavaScript:handleAppend(this,'appendVs" + i + "')\">Select</button> <video id=\"video1\" width=\"300\" height=\"220\" controls> <source src=\""+url+"\"type=\"video/ogg\" <\/video> </div>";
-		output = output + "<div id=\"text" + actor + "" + phrase+ "\"class=\"" + actor.toUpperCase() + " " + phrase.toUpperCase()+ "\"> <p> Character: "+actor+"<br>Phrase: "+phrase+" </p> </div> ";
-	}
-	
-	videoSegSearch.innerHTML= output;
-	//console.log(output);
-}
-*/
